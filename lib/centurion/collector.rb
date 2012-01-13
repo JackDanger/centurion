b8ae195bd0be96bf28dd23c91150e1ea68bce188
Jack Danger <jackdanger@squareup.com>
2012-01-13 08:40:08 -0800

    clip works

--- lib/centurion/collector.rb
+++ lib/centurion/collector.rb
@@ -5,20 +5,24 @@ module Centurion
     attr_reader :project_root, :project_name,
                 :repo, :bucket
 
-    def initialize project_root
-      @project_root = project_root
+    def initialize options
+      p options
+      @project_root = options[:project_root]
+      @commit_range = options[:commit_range]
       @repo  = Grit::Repo.new project_root
       @project_name = File.basename project_root
-      @bucket = Centurion.db.bucket(@project_name)
+      @bucket = Centurion.db.bucket(project_name)
     end
 
-    def meter ref
-      puts "Collecting #{ref} in #{project_name}"
-      files.each_with_index do |file, idx|
-        Flog.new(file, commit_data_for(ref)).meter do |data|
-          insert data
+    def meter
+      each_commit do |ref|
+        puts "Collecting #{ref} in #{project_name}"
+        each_file do |file, idx|
+          Flog.new(file, commit_data_for(ref)).meter do |data|
+            insert data
+          end
+          puts "processed #{idx+1}/#{files.size} - #{file.sub(/^#{project_root}\//,'')}"
         end
-        puts "processed #{idx+1}/#{files.size} - #{file.sub(/^#{project_root}\//,'')}"
       end
     end
 
@@ -40,11 +44,18 @@ module Centurion
       }
     end
 
-    def files
-      @files ||= begin
-        found = Dir.glob File.join(project_root, '**/*.rb')
-        warn "No Ruby source files found in #{project_root}!" if found.empty?
-        found.map {|file| file.sub(/^#{project_root}\//, '') }
+    def each_commit
+      @repo.commits(@commit_range).each do |commit|
+        yield commit
+      end
+    end
+
+    def each_file
+      found = Dir.glob File.join(project_root, '**/*.rb')
+      warn "No Ruby source files found in #{project_root}!" if found.empty?
+      found.each_with_index do |file, idx|
+        path = file.sub(/^#{project_root}\//, '')
+        yield path, idx
       end
     end
   end
