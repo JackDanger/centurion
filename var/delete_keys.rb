849a41001d6d27a55173b867f01e2d8def81b45f
Jack Danger <jackdanger@squareup.com>
2012-01-21 08:17:57 -0800

    deleting within the specified bucket

--- var/delete_keys.rb
+++ var/delete_keys.rb
@@ -3,7 +3,7 @@ gem 'yajl-ruby'
 require 'riak'
 require 'yajl'
 riak = Riak::Client.new
-bucket = riak.bucket('centurion')
+bucket = riak.bucket(ENV['bucket'])
 Yajl::Parser.parse(STDIN) do |set|
-  set['keys'] && set['keys'].each {|key| puts bucket.delete(key) }
+  set['keys'] && set['keys'].each {|key| p bucket.delete(key) }
 end
