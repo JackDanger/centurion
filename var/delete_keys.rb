da70027eb83113141ee181f1e975554635a17779
Jack Danger <jackdanger@squareup.com>
2012-03-01 19:58:00 -0800

    fixing the bucket cleaner

--- var/delete_keys.rb
+++ var/delete_keys.rb
@@ -4,6 +4,7 @@ require 'riak'
 require 'yajl'
 riak = Riak::Client.new
 bucket = riak.bucket(ENV['bucket'])
-Yajl::Parser.parse(STDIN) do |set|
-  set['keys'] && set['keys'].each {|key| p bucket.delete(key) }
+bucket.keys do |set|
+  p set
+  set.each {|key| p bucket.delete(key) }
 end
